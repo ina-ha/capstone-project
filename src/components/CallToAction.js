b13b1d5c888151754a0ebb3335e2dedd1b27db4f
ina-ha <ina.haidava.tech@gmail.com>
2023-07-26 13:11:06 +0200

    added specials section

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -9,7 +9,7 @@ function CallToAction() {
     <div className='action-text'>
     <h1>Little Lemon</h1> 
     <h2>Chicago</h2>
-    <h3>We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist</h3>
+    <h3>We are a family-owned Mediterranean restaurant,<br/> focused on traditional recipes served with a modern twist</h3>
     <button className='btn'>{<Link className='btn-link' to='/booking'>Reserve a Table</Link>}</button>
     </div>
     <div className='action-image'>
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CallToAction from './CallToAction';
+import Specials from './Specials';
 
 function Homepage() {
   return (
     <main>
     <CallToAction />
+    <Specials />
     </main>
   )
 }
